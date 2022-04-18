3321b500077bd3842e4968303b00b1c4f7949f0e
Kiran <kiranhulawale123@gmail.com>
2022-04-18 19:35:30 +0530

    Added UC-2 to calculate daily wages

--- EmployeeWagesCalculation.js
+++ EmployeeWagesCalculation.js
@@ -1,11 +1,12 @@
+let EMP_RATE_PER_HOUR = 20;
+
+// Variables
 let isPresent = 1;
-let empCheck = (Math.floor(Math.random() * 10) % 2);
-
-if (empCheck == 1) {
-    console.log("Employee is Present");
-   // return;
-} else {
-
-    console.log("Employee is Absent");
-
-}
+let empCheck = Math.floor(Math.random() * 10) % 2;
+let empHrs = 0, empWage = 0;
+if (empCheck == 1)
+    empHrs = 8;
+else
+    empHrs = 0;
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log("Employee Wage = "+empWage);
